a473c5353d13bae577223a765ff698303ee2b639
Pavel Volchenko <2793553@gmail.com>
2022-09-23 18:11:16 +0300

    Обновил решение задачи палиндрома на более кароткую запись. Просто развернул входящее число.

--- Task019/Program.cs
+++ Task019/Program.cs
@@ -2,36 +2,56 @@
     пятизначное число и проверяет, является ли оно палиндромом.
     14212 -> нет
     12821 -> да
-    23432 -> да                                              */    
-int backOfNumber(int number)
+    23432 -> да                                              */
+
+int reversInputNumber(int number)
 {
-    int backOfNumber = number;
-    backOfNumber %= 100;
-    return backOfNumber;
+    int reverseNumber = 0;
+    reverseNumber = ((number % 10) * 10000) + (((number / 10) % 10) * 1000) + 
+    (((number / 100) % 10) * 100) + (((number / 1000) % 10) * 10) + (number / 10000);    
+    return reverseNumber;
 }
 
-int frontOfNumber(int number)
-{
-    int frontOfNumber = number;
-    frontOfNumber /= 1000;
-    return frontOfNumber;
-}
-
-int reverseBackOfNumber(int backOfNumber)
-{
-    int result = backOfNumber % 10;
-    result *= 10;
-    backOfNumber /= 10;
-    result = result + backOfNumber;
-    return result;
-}
-
-int number = 14252;
-int toReverseNumber = backOfNumber(number);
+int number = 14841;
 Console.WriteLine("Входное число: " + number);
 
-if(reverseBackOfNumber(toReverseNumber) == frontOfNumber(number)) 
+if(reversInputNumber(number) == number) 
 {
     Console.WriteLine("ДА! Это число палиндром!");
 }
 else Console.WriteLine("НЕТ! Это число не является палиндромом!");
+
+
+// int backOfNumber(int number)
+// {
+//     int backOfNumber = number;
+//     backOfNumber %= 100;
+//     return backOfNumber;
+// }
+
+
+// int frontOfNumber(int number)
+// {
+//     int frontOfNumber = number;
+//     frontOfNumber /= 1000;
+//     return frontOfNumber;
+// }
+
+// int reverseBackOfNumber(int backOfNumber)
+// {
+//     int result = backOfNumber % 10;
+//     result *= 10;
+//     backOfNumber /= 10;
+//     result = result + backOfNumber;
+//     return result;
+// }
+
+// int number = 14252;
+// int toReverseNumber = backOfNumber(number);
+// Console.WriteLine("Входное число: " + number);
+
+// if(reverseBackOfNumber(toReverseNumber) == frontOfNumber(number)) 
+// {
+//     Console.WriteLine("ДА! Это число палиндром!");
+// }
+// else Console.WriteLine("НЕТ! Это число не является палиндромом!");
