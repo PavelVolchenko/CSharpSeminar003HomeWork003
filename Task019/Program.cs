084529a787a427700a6f3f22ee27522bb5fb3792
Pavel Volchenko <2793553@gmail.com>
2022-09-23 15:30:21 +0300

    Init

---
+++ Task019/Program.cs
@@ -0,0 +1,42 @@
+﻿/*  Задача 19: Напишите программу, которая принимает на вход 
+    пятизначное число и проверяет, является ли оно палиндромом.
+    14212 -> нет
+    12821 -> да
+    23432 -> да                                              */
+
+int backOfNumber(int number)
+{
+    int backOfNumber = number;
+    backOfNumber %= 100;
+    return backOfNumber;
+}
+
+int frontOfNumber(int number)
+{
+    int frontOfNumber = number;
+    frontOfNumber /= 1000;
+    return frontOfNumber;
+}
+
+int reverseBackOfNumber(int backOfNumber)
+{
+    int result = backOfNumber % 10;
+    result *= 10;
+    backOfNumber /= 10;
+    result = result + backOfNumber;
+    return result;
+}
+
+int number = 14241;
+int toReverseNumber = backOfNumber(number);
+
+Console.WriteLine("Входное число: " + number);
+Console.WriteLine("Передняя часть числа: " + frontOfNumber(number));
+Console.WriteLine("Задняя часть числа: " + backOfNumber(number));
+Console.WriteLine("Перевернутая часть заднего числа: " + reverseBackOfNumber(toReverseNumber));
+
+if(reverseBackOfNumber(toReverseNumber) == frontOfNumber(number)) 
+{
+    Console.WriteLine("ДА! Это число палиндром!");
+}
+else Console.WriteLine("НЕТ! Это число не является палиндромом!");
